83a5c132de7173b55a55c2c1372a193ca910d5e9
Alyssa Diaz <alydiaz77@gmail.com>
2017-02-07 17:24:03 -0800

    finished

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 
+  $("#blue").on("click", function(){
+    var property = $(this).parent().siblings(":first").text();
+    var value = $(this).parent().prev().text();
+    $(this).closest("tr").children().css(property, value);
+  });
 
+
+  $("tbody > tr:nth-child(2) button").on("click", function(){
+    var property = $(this).parent().siblings(":first").text();
+    var value = $(this).parent().prev().text();
+    $(this).closest("tbody").children(":first").css(property, value);
+  });
+
+  $("#stuff > h2").on("mouseover", function(){
+
+    $(this).next().find(".list:first").css("font-weight", "bold");
+  })
 });
 
 
